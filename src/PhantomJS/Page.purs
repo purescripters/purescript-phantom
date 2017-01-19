3ae2e1016b982f81db90281bed9b97b34ccc28b1
Dominick Gendill <dgendill@gmail.com>
2017-01-19 12:13:11 -0700

    Added phantomjs effect to functions in Page.purs

--- src/PhantomJS/Page.purs
+++ src/PhantomJS/Page.purs
@@ -17,10 +17,12 @@ import Control.Monad.Aff (Aff)
 import Data.Tuple (Tuple)
 import Data.StrMap (StrMap, fromFoldable)
 import Data.Foldable (class Foldable)
+import PhantomJS.Phantom (PHANTOMJS)
 
 type URL = String
 type FilePath = String
 type RenderQuality = Int
+type PhantomAff e a = Aff (phantomjs :: PHANTOMJS | e) a
 
 -- | The type of image format when rendering a screenshot
 data RenderFormat
@@ -62,48 +64,48 @@ png
 -- | The type of a PhantomJS page context.
 foreign import data Page :: *
 
-foreign import createPage_ :: forall e. Aff (| e) Page
+foreign import createPage_ :: forall e. PhantomAff e Page
 
 -- | Creates a new page context.
-createPage :: forall e. Aff e Page
+createPage :: forall e. PhantomAff e Page
 createPage = createPage_
 
 
-foreign import customHeaders_ :: forall e. Page -> StrMap String -> Aff (| e) Page
+foreign import customHeaders_ :: forall e. Page -> StrMap String -> PhantomAff e Page
 
 -- | Sets custom headers on a page context.  These headers will persist
 -- | for the life of the context.
-customHeadersRaw :: forall e f. (Foldable f) => Page -> f (Tuple String String) -> Aff e Page
+customHeadersRaw :: forall e f. (Foldable f) => Page -> f (Tuple String String) -> PhantomAff e Page
 customHeadersRaw page headers = customHeaders_ page (fromFoldable headers)
 
 
-foreign import open_ :: forall e. Page -> URL -> Aff (| e) Page
+foreign import open_ :: forall e. Page -> URL -> PhantomAff e Page
 
 -- | Opens a URL in a page context.
-open :: forall e. Page -> URL -> Aff e Page
+open :: forall e. Page -> URL -> PhantomAff e Page
 open = open_
 
 
-foreign import render_ :: forall e. Page -> FilePath -> String -> RenderQuality -> Aff (| e) Page
+foreign import render_ :: forall e. Page -> FilePath -> String -> RenderQuality -> PhantomAff e Page
 
 -- | Renders a screenshot of the given page.
-render :: forall e. Page -> FilePath -> RenderSettings ->  Aff e Page
+render :: forall e. Page -> FilePath -> RenderSettings ->  PhantomAff e Page
 render page fp (RenderSettings { format : format, quality : quality }) = render_ page fp (show format) quality
 
 
-foreign import injectJs_ :: forall e. Page -> FilePath -> Aff (| e) Page
+foreign import injectJs_ :: forall e. Page -> FilePath -> PhantomAff e Page
 
 -- | Injects a javascript file into a page.
-injectJs :: forall e. Page -> FilePath -> Aff e Page
+injectJs :: forall e. Page -> FilePath -> PhantomAff e Page
 injectJs = injectJs_
 
 
-foreign import evaluate_ :: forall e a. Page -> String -> Aff (| e) a
+foreign import evaluate_ :: forall e a. Page -> String -> PhantomAff e a
 
 -- | Evaluates a specific function on the window object of the page.
 -- | When using this function you will need to add a type annotation so
 -- | PureScript knows what type the native javascript will return, e.g.
 -- |
 -- | `result <- evaluate page "file.js" :: forall e. Aff e (Array String)`
-evaluate :: forall e a. Page -> String -> Aff e a
+evaluate :: forall e a. Page -> String -> PhantomAff e a
 evaluate = evaluate_
