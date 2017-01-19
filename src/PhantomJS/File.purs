35a1946a7b42d5304391c3f8a6a4f91f07c3cd33
Dominick Gendill <dgendill@gmail.com>
2017-01-19 10:39:43 -0700

    Generic and foreign work

--- src/PhantomJS/File.purs
+++ src/PhantomJS/File.purs
@@ -1,41 +1,47 @@
 module PhantomJS.File where
 
-import Prelude (Unit, class Eq, class Show)
+import Prelude (Unit, class Eq, class Show, show, (<<<))
 import Data.Function.Uncurried (Fn2, runFn2)
 import Control.Monad.Aff (Aff)
-import Data.Foreign (toForeign)
+import Data.Foreign (toForeign, Foreign)
 import Data.Foreign.Class (class AsForeign, write)
-import Data.Generic (class Generic)
+import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 
 type Charset = String
 type FilePath = String
+type ForeignFileSettings = Foreign
 
-data FileMode
-  = R
-  | W
-  | A
-  | B
+data FileMode = R | W | A | B
+derive instance genericFileMode :: Generic FileMode _
 
 derive instance eqFileMode :: Eq FileMode
-derive instance genericFileMode :: Generic FileMode
+
 instance showFileMode :: Show FileMode where
-  show x = genericShow x
+  show R = "r"
+  show W = "w"
+  show A = "a/+"
+  show B = "b"
 
 instance foreignFileMode :: AsForeign FileMode where
-  write R = toForeign "r"
-  write W = toForeign "w"
-  write A = toForeign "a/+"
-  write B = toForeign "b"
+  write = toForeign <<< show
 
+instance asForeignFileSettings :: AsForeign FileSettings where
+  write (FileSettings { mode : filemode, charset : charset }) = toForeign
+    { mode : (write filemode)
+    , charset : (write charset) }
 
-newtype FileSettings
-  = FileSettings
+-- http://www.iana.org/assignments/character-sets/character-sets.xhtml
+newtype FileSettings = FileSettings
   { mode :: FileMode
-  -- http://www.iana.org/assignments/character-sets/character-sets.xhtml
-  , charset :: String }
+  , charset :: String
+  }
 
-derive instance asForeignFileSettings :: AsForeign FileSettings
+derive instance genericFileSettings :: Generic FileSettings _
+
+instance showFileSettings :: Show FileSettings where
+  show x = genericShow x
+
 
 forWritingIn :: Charset -> FileSettings
 forWritingIn charset =
@@ -57,7 +63,7 @@ foreign import data FileStream :: *
 
 type PhantomAff e a = Aff ( phantomfs :: PHANTOMFS | e ) a
 
-foreign import openStream_ :: forall e. Fn2 FilePath FileSettings (PhantomAff e FileStream)
+foreign import openStream_ :: forall e. Fn2 FilePath ForeignFileSettings (PhantomAff e FileStream)
 
 foreign import writeStream_ :: forall e.  Fn2 FileStream String (PhantomAff e FileStream)
 
